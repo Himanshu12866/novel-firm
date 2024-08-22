ea9bb42b06201c7d65d86e5336d3f347a957dc7d
Himanshu12866 <manshu010m@gmail.com>
2024-08-22 16:06:50 +0530

    "Added Montserrat font to App.css, imported Header component in App.js, and created new Header component with HTML structure and CSS import"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css"
+import Header from "./components/header";
 export default function App(){
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+
+import "../styles/header.css"
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="text-box">
+                <h2>We Innovate The</h2>
+                <h2>Future Planet Just</h2>
+                <h2>BY Data</h2>
+
+            </div>
+
+        </div>
+    )
+}
