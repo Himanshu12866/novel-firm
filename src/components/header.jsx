a8a28a45d9826a3434dcb6fb4437e24771f09af8
Himanshu12866 <manshu010m@gmail.com>
2024-08-22 18:19:50 +0530

    "Added video overlay feature to header component, including styles and layout changes for desktop and mobile views."

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,42 @@
 
+import { useState } from "react"
 import "../styles/header.css"
 import NavBar from "./navlinks"
 export default function Header() {
+    const [styelObj,setStyle] = useState("overlay-vdo1")
+    function ChangeStyle(){
+        setStyle("overlay-vdo")
+    }
+function Unchange(){
+    setStyle("overlay-vdo1")
+}
     return (
         <div className="header">
+        <div className={styelObj}>
+                <button onClick={Unchange} className="btn btn-close"></button>
+               <iframe width="100%" height="100%" src="https://www.youtube.com/embed/FYHxewmN9FQ?si=8sJRGL1y8ZIjDRp5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
             <NavBar />
             <div className="text-box">
                 <div>
                     <h2>We Innovate The</h2>
                     <h2>Future Planet Just</h2>
-                    <h2>BY Data</h2>
+                    <h2>By Data</h2>
+
+                    <p><b>Massive Dynamic</b> --
+                        <span>The only theme covers all aspects;</span>
+                        <br></br>
+                        <span>beautiful layouts, super customizable, blazing fast, </span>
+                        <br></br>
+                        <span>customer first approach.</span>
+                    </p>
+                    <div className="play-btn">
+                        <button onClick={ChangeStyle}><span className="bi bi-play-fill"></span></button>
+                        <a href="/">Discover Our Magic</a>
+                    </div>
                 </div>
             </div>
+            
 
         </div>
     )
