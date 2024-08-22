53480fbbe35f5a355925d82ac1c70083c78a5e11
Himanshu12866 <manshu010m@gmail.com>
2024-08-22 17:27:48 +0530

    "Updated Header and Navlinks components with minor layout and styling changes"

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,13 +4,13 @@ import NavBar from "./navlinks"
 export default function Header() {
     return (
         <div className="header">
-        <NavBar />
+            <NavBar />
             <div className="text-box">
-
-                <h2>We Innovate The</h2>
-                <h2>Future Planet Just</h2>
-                <h2>BY Data</h2>
-
+                <div>
+                    <h2>We Innovate The</h2>
+                    <h2>Future Planet Just</h2>
+                    <h2>BY Data</h2>
+                </div>
             </div>
 
         </div>
--- src/components/navlinks.jsx
+++ src/components/navlinks.jsx
@@ -1,36 +1,48 @@
-
-import "../styles/navlinks.css"
+import "../styles/navlinks.css";
 export default function NavBar() {
-    return (
-        <div className="navbar navbar-expand-sm">
-            <a href="/" className="navbar-brand"> Jenga</a>
-            <div className="bi bi-bag"></div>
-            <button className="navbar-toggler" data-bs-target="#links" data-bs-toggle="collapse">
-
-                <span className="bi bi-list"></span>
-            </button>
-            <div className="navbar-collapse collapse" id="links">
-                <ol className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">Services</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">Works</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">Team</a>
-                    </li>
-                    <li className="nav-item input-box p-3">
-                        <div className="input-group">
-                            <input type="text" className="form-control" placeholder="Search..."></input>
-                            <span className="bi bi-search input-group-text"></span>
-                        </div>
-                    </li>
-
-                </ol>
-
+  return (
+    <div className="navbar navbar-expand-sm">
+      <a href="/" className="navbar-brand">
+      
+        Jenga
+      </a>
+      <div className="bi bi-bag"></div>
+      <button
+        className="navbar-toggler"
+        data-bs-target="#links"
+        data-bs-toggle="collapse"
+      >
+        <span className="bi bi-list"></span>
+      </button>
+      <div className="navbar-collapse collapse" id="links">
+        <ol className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Services
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Works
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Team
+            </a>
+          </li>
+          <li className="nav-item input-box p-3">
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search..."
+              ></input>
+              <span className="bi bi-search input-group-text"></span>
             </div>
-
-        </div>
-    )
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
