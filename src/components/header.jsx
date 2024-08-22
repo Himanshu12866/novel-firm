f2e70310892ce19f5d84188ddc5d6945956b4bdd
Himanshu12866 <manshu010m@gmail.com>
2024-08-22 18:58:10 +0530

    "Updated App.js, header.jsx, header.css, and header.scss files with minor changes to layout, styling, and animation."

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,7 +13,7 @@ function Unchange(){
     return (
         <div className="header">
         <div className={styelObj}>
-                <button onClick={Unchange} className="btn btn-close"></button>
+                <button onClick={Unchange} className="btn  text-light"><span className=" bi bi-x-lg"></span></button>
                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/FYHxewmN9FQ?si=8sJRGL1y8ZIjDRp5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
             <NavBar />
