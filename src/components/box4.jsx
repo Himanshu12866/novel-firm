6c54ae3d5e1ea0bdc26cd53c9f7f54a22bf86cfe
Himanshu12866 <manshu010m@gmail.com>
2024-08-23 18:37:52 +0530

    "Updated Box4 component with new text boxes and styling, added font import, and modified CSS for responsiveness"

--- src/components/box4.jsx
+++ src/components/box4.jsx
@@ -6,13 +6,24 @@ export default function Box4() {
             <div className="row">
                 <div className="col-lg-4 col-sm-12">
                     <img alt="pics" src="../images/pic3.jpg"></img>
-                    <div></div>
+                    <div className="text-box">
+                        <h4>Lallan Pathan</h4>
+                        <p>Founder</p>
+                    </div>
                 </div>
                 <div className="col-lg-4 col-sm-12">
                     <img alt="pics" src="../images/pic4.jpg"></img>
+                    <div className="text-box">
+                        <h4>Lotiya Pathan</h4>
+                        <p>CEO & Executive</p>
+                    </div>
                 </div>
                 <div className="col-lg-4 col-sm-12">
                     <img alt="pics" src="../images/pic5.jpg"></img>
+                    <div className="text-box">
+                        <h4>Nimbu Hatela</h4>
+                        <p>Software Engineer</p>
+                    </div>
                 </div>
             </div>
 
