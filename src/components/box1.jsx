354048dfd96139135fb9199f5c3a39b847ba5bd0
Himanshu12866 <manshu010m@gmail.com>
2024-08-23 15:23:47 +0530

    "Updated Box1 component and styles: refactored JSX structure, added new HTML elements, and modified CSS rules for layout, typography, and responsiveness."

--- src/components/box1.jsx
+++ src/components/box1.jsx
@@ -1,18 +1,20 @@
 
 import "../styles/box1.css"
-export default function Box1(){
+export default function Box1() {
     return (
         <div className="box1">
-           <div className="row">
             <div className="row">
-                <div className="col-lg-1 col-sm-12">
-                    <img src="../images/camera.png" alt="camera"></img>
+                <div className="row">
+                    <div className="col-lg-1 col-sm-12">
+                        <img src="../images/camera.png" alt="camera"></img>
+                    </div>
+                    <div className="col-lg-11 col-sm-12">
+                        <p>All Services and Sulutions</p>
+                    </div>
                 </div>
-                <div className="col-lg-11 col-sm-12">
-                    <p>All Services and Sulutions</p>
-                </div>
+                <h1>We provide best branding and</h1>
+                <h1 style={{color:"#481DFF",fontWeight:"bolder"}}>live events solutions —</h1>
             </div>
-           </div>
         </div>
     )
 }
