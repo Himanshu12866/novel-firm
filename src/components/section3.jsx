c692d94a5d10b49b821097c1bda15b9ba5bfb37d
Himanshu12866 <manshu010m@gmail.com>
2024-08-23 17:41:17 +0530

    "Updated SectionBox3 component and corresponding CSS styles"

--- src/components/section3.jsx
+++ src/components/section3.jsx
@@ -1,30 +1,77 @@
 
 import "../styles/section3.css"
-export default function SectionBox3(){
+export default function SectionBox3() {
     return (
         <div className="section-box3">
             <div className="row">
                 <div className="col-lg-3 col-sm-12">
                     <img alt="pic" src="../images/pic14.jpg"></img>
                     <div className="overlayBox">
-                       <div className="innerBox">
-                        <img alt="icon" src="../images/icon1.png"></img>
-                        <div className="innerBox1">
-                            <h1>Clike me</h1>
+                        <div className="innerBox">
+                            <img alt="icon" src="../images/icon1.png"></img>
+                            <div className="innerBox1">
+                                <h2>Build it</h2>
+                                <h2>Quick.</h2>
+                                <p>
+                                    No more fumbling to unlock your phone and find your notes app; Cheatsheet is as easy as pulling down center, pulling up the keyboard, or glancing at your Apple Watch.
+                                </p>
+                                <button className="rounded rounded-pill">How To Begin</button>
+                            </div>
                         </div>
-                       </div>
-                      
+
                     </div>
 
                 </div>
                 <div className="col-lg-3 col-sm-12">
                     <img alt="pic" src="../images/pic1.jpg"></img>
+                    <div className="overlayBox">
+                        <div className="innerBox">
+                            <img alt="icon" src="../images/icon2.png"></img>
+                            <div className="innerBox1">
+                               <div className="text-box">
+                               <h2>Customize</h2>
+                               <h2>Everything</h2>
+
+                               </div>
+                              
+                                <button className="rounded rounded-pill">Go To Website</button>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
                 <div className="col-lg-3 col-sm-12">
                     <img alt="pic" src="../images/pic150.jpg"></img>
+                    <div className="overlayBox">
+                        <div className="innerBox">
+                            <img alt="icon" src="../images/icon3.png"></img>
+                            <div className="innerBox1">
+                                <h2>And We Blog</h2>
+                                <h2>Sometimes.</h2>
+                                <p>
+                                    No more fumbling to unlock your phone and find your notes app; Cheatsheet is as easy as pulling down center, pulling up the keyboard, or glancing at your Apple Watch.
+                                </p>
+                                <button className="rounded rounded-pill">See the Blog</button>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
                 <div className="col-lg-3 col-sm-12">
                     <img alt="pic" src="../images/pic2.jpg"></img>
+                    <div className="overlayBox">
+                        <div className="innerBox">
+                            <img alt="icon" src="../images/icon4.png"></img>
+                            <div className="innerBox1">
+                            <div className="text-box">
+                               <h2>Customize</h2>
+                               <h2>Everything</h2>
+
+                               </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
