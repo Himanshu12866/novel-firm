576ac3b8748b2385939632902699570f458bf853
Himanshu12866 <manshu010m@gmail.com>
2024-08-23 18:23:35 +0530

    "Added Box4 import and component to SectionBox4"

---
+++ src/components/box4.jsx
@@ -0,0 +1,21 @@
+
+import "../styles/box4.css"
+export default function Box4() {
+    return (
+        <div className="box4">
+            <div className="row">
+                <div className="col-lg-4 col-sm-12">
+                    <img alt="pics" src="../images/pic3.jpg"></img>
+                    <div></div>
+                </div>
+                <div className="col-lg-4 col-sm-12">
+                    <img alt="pics" src="../images/pic4.jpg"></img>
+                </div>
+                <div className="col-lg-4 col-sm-12">
+                    <img alt="pics" src="../images/pic5.jpg"></img>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- src/components/section4.jsx
+++ src/components/section4.jsx
@@ -1,11 +1,13 @@
 import Box3 from "./box3";
+import Box4 from "./box4";
 
 
 
-export default function SectionBox4(){
+export default function SectionBox4() {
     return (
         <div className="sectionbox1">
-       <Box3/>
+            <Box3 />
+            <Box4 />
 
         </div>
     )
