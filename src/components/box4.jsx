acef9f9ca807f59e684f093f278331e425bf689c
Himanshu12866 <manshu010m@gmail.com>
2024-08-24 16:31:05 +0530

    "Added separate state variables and functions for each slider, updated CSS to include cursor pointer, and reformatted code"

--- src/components/box4.jsx
+++ src/components/box4.jsx
@@ -3,13 +3,28 @@ import { useState } from "react"
 import "../styles/box4.css"
 export default function Box4() {
     const [dstyle, setStyle] = useState("slider-1")
-    function AddStyle() {
+    const [dstyle1, setStyle1] = useState("slider-1")
+    const [dstyle2, setStyle2] = useState("slider-1")
+    function AddStyle(e) {
+
         setStyle("slider")
 
     }
     function RemoveStyle() {
         setStyle("slider-1");
 
+    }
+    function AddStyle1() {
+        setStyle1("slider")
+    }
+    function RemoveStyle1() {
+        setStyle1("slider-1")
+    }
+    function AddStyle2() {
+        setStyle2("slider")
+    }
+    function RemoveStyle2() {
+        setStyle2("slider-1")
     }
     return (
         <div className="box4">
@@ -38,17 +53,17 @@ export default function Box4() {
                 </div>
                 <div className="col-lg-4 col-sm-12">
                     <img alt="pics" src="../images/pic4.jpg"></img>
-                    <div className="menu d-flex justify-content-center align-items-center" onClick={AddStyle}>
+                    <div className="menu d-flex justify-content-center align-items-center" onClick={AddStyle1}>
                         <span className="bi  bi-list-nested"></span>
                     </div>
                     <div className="text-box">
                         <h4>Lotiya Pathan</h4>
                         <p>CEO & Executive</p>
                     </div>
-                    <div className={dstyle}>
-                        <b className="bi bi-x-lg" id="slider" onClick={RemoveStyle}></b>
-                        <h2>Lallan Pathan</h2>
-                        <p>Founder</p>
+                    <div className={dstyle1}>
+                        <b className="bi bi-x-lg" onClick={RemoveStyle1}></b>
+                        <h2>Lotiya Pathan</h2>
+                        <p>CEO & Executive</p>
                         <br></br>
                         <i>A web application, or web app, is a program that is stored on a remote server and delivered to a user's device through a web browser interface.</i>
                         <div className="d-flex justify-content-around align-items-center mt-3">
@@ -60,17 +75,17 @@ export default function Box4() {
                 </div>
                 <div className="col-lg-4 col-sm-12">
                     <img alt="pics" src="../images/pic5.jpg"></img>
-                    <div className="menu d-flex justify-content-center align-items-center" onClick={AddStyle}>
+                    <div className="menu d-flex justify-content-center align-items-center" onClick={AddStyle2}>
                         <span className="bi  bi-list-nested"></span>
                     </div>
                     <div className="text-box">
                         <h4>Nimbu Hatela</h4>
                         <p>Software Engineer</p>
                     </div>
-                    <div className={dstyle}>
-                        <b className="bi bi-x-lg" onClick={RemoveStyle}></b>
-                        <h2>Lallan Pathan</h2>
-                        <p>Founder</p>
+                    <div className={dstyle2}>
+                        <b className="bi bi-x-lg" onClick={RemoveStyle2}></b>
+                        <h2>Nimbu Hatela</h2>
+                        <p>Software Engineer</p>
                         <br></br>
                         <i>A web application, or web app, is a program that is stored on a remote server and delivered to a user's device through a web browser interface.</i>
                         <div className="d-flex justify-content-around align-items-center mt-3">
