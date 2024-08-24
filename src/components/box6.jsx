a3bf6d31ede9bd423a2835f3264aaadf79bad1f4
Himanshu12866 <manshu010m@gmail.com>
2024-08-24 17:58:29 +0530

    "Formatted JSX and SCSS code in box6 component and styles"

--- src/components/box6.jsx
+++ src/components/box6.jsx
@@ -1,6 +1,6 @@
 
 import "../styles/box6.css"
-export default function Box6(){
+export default function Box6() {
     return (
         <div className="box6">
             <div className="row">
@@ -9,21 +9,21 @@ export default function Box6(){
                         <div className="col-12">
                             <img src="images/pic6.jpg" alt="kuchv"></img>
                             <div className="overlay-box">
-                            <div></div>
-                               <div>
-                               <p style={{color:"#ff41a0"}}>— Startup Solution</p>
-                               <h1>Moonlight Co.</h1>
-                               </div>
+                                <div></div>
+                                <div>
+                                    <p style={{ color: "#ff41a0" }}>— Startup Solution</p>
+                                    <h1>Moonlight Co.</h1>
+                                </div>
                             </div>
                         </div>
                         <div className="col-12">
-                        <img src="images/pic7.jpg" alt="kuchv"></img>
-                        <div className="overlay-box">
-                            <div></div>
-                               <div>
-                               <p style={{color:"#ff41a0"}}>— Startup Solution</p>
-                               <h1>THE VOUGE</h1>
-                               </div>
+                            <img src="images/pic7.jpg" alt="kuchv"></img>
+                            <div className="overlay-box">
+                                <div></div>
+                                <div>
+                                    <p style={{ color: "#ff41a0" }}>— Startup Solution</p>
+                                    <h1>THE VOUGE</h1>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -31,48 +31,48 @@ export default function Box6(){
                 <div className="col-lg-4 col-sm-12">
                     <div className="row">
                         <div className="col-12">
-                        <div className="overlay-box">
-                            <div></div>
-                               <div>
-                               <p style={{color:"#ff41a0"}}>— Magezine</p>
-                               <h1>Ryan Ryenolds</h1>
-                               </div>
+                            <div className="overlay-box">
+                                <div></div>
+                                <div>
+                                    <p style={{ color: "#ff41a0" }}>— Magezine</p>
+                                    <h1>Ryan Ryenolds</h1>
+                                </div>
                             </div>
                             <img src="images/pic8.jpg" alt="kuchv"></img>
                         </div>
                         <div className="col-12">
-                        <div className="overlay-box">
-                            <div></div>
-                               <div>
-                               <p style={{color:"#ff41a0"}}>— Travelling</p>
-                               <h1>The Himalaya</h1>
-                               </div>
+                            <div className="overlay-box">
+                                <div></div>
+                                <div>
+                                    <p style={{ color: "#ff41a0" }}>— Travelling</p>
+                                    <h1>The Himalaya</h1>
+                                </div>
                             </div>
-                        <img src="images/pic9.jpg" alt="kuchv"></img>
+                            <img src="images/pic9.jpg" alt="kuchv"></img>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-12 col-1">
                     <div className="row">
                         <div className="col-12">
-                        <div className="overlay-box">
-                            <div></div>
-                               <div>
-                               <p style={{color:"#ff41a0"}}>— Live Events</p>
-                               <h1>Selena Gomez.</h1>
-                               </div>
+                            <div className="overlay-box">
+                                <div></div>
+                                <div>
+                                    <p style={{ color: "#ff41a0" }}>— Live Events</p>
+                                    <h1>Selena Gomez.</h1>
+                                </div>
                             </div>
                             <img src="images/pic10.jpg" alt="kuchv"></img>
                         </div>
                         <div className="col-12">
-                        <div className="overlay-box">
-                            <div></div>
-                               <div>
-                               <p style={{color:"#ff41a0"}}>— Startup Solution</p>
-                               <h1>Virat Kohli</h1>
-                               </div>
+                            <div className="overlay-box">
+                                <div></div>
+                                <div>
+                                    <p style={{ color: "#ff41a0" }}>— Startup Solution</p>
+                                    <h1>Virat Kohli</h1>
+                                </div>
                             </div>
-                        <img src="images/pic11.jpg" alt="kuchv"></img>
+                            <img src="images/pic11.jpg" alt="kuchv"></img>
                         </div>
                     </div>
                 </div>
