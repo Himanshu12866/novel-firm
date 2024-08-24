bf3d5bead9128a2af1c08589d8f76b27253faae4
Himanshu12866 <manshu010m@gmail.com>
2024-08-24 18:39:59 +0530

    "Updated Section6 component and styles: added div wrappers, modified button styles and layout, and updated CSS mappings."

--- src/components/section6.jsx
+++ src/components/section6.jsx
@@ -1,15 +1,23 @@
 
 import "../styles/section6.css"
-export default function SectionBox6(){
+export default function SectionBox6() {
     return (
         <div className="section-box6">
             <div className="row">
                 <div className="col-lg-6 col-sm-12">
-                    <h2>Make your website high quality </h2>
-                    <h2>Join the Pixflow community.</h2>
+                    <div>
+                        <h2>Make your website high quality </h2>
+                        <h2>Join the Pixflow community.</h2>
+                    </div>
                 </div>
-                <div className="col-lg-6 col-sm-12" style={{textAlign:"right"}}>
-                    <button >Live Chat With The Team</button>
+                <div className="col-lg-6 col-sm-12" style={{ textAlign: "right" }}>
+                    <div>
+                        <button >Live Chat With The Team</button>
+                        <div>
+                            <p>You any questions ?</p>
+                            <p>Ask here.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
