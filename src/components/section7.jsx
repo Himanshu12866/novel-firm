172c8ade4c0e5a177dc6b49f0f9d547a3b0e6fb9
Himanshu12866 <manshu010m@gmail.com>
2024-08-28 14:54:20 +0530

    "Updated SectionBox7 component and section7.css/scss files with minor formatting changes and adjustments to styles and layout."

--- src/components/section7.jsx
+++ src/components/section7.jsx
@@ -1,18 +1,18 @@
 
 import "../styles/section7.css"
-export default function SectionBox7(){
+export default function SectionBox7() {
     return (
         <div className="section-box7">
-        <div className="row">
+            <div className="row">
 
-        <div className="col-lg-6 col-sm-12">
+                <div className="col-lg-6 col-sm-12">
 
-        </div>
-        <div  id="img-box" className="col-lg-6 col-sm-12">
-        <img src="/images/pic13.jpg" alt="img"></img>
+                </div>
+                <div id="img-box" className="col-lg-6 col-sm-12">
 
-        </div>
-        </div>
+
+                </div>
+            </div>
 
 
         </div>
