73d2705094b785a34cc2a1192201e3e4628c66e3
Himanshu12866 <manshu010m@gmail.com>
2024-08-28 15:05:47 +0530

    "Updated Section7 component and styles: added self-referential component call, modified CSS to center content, and updated CSS map file."

---
+++ src/components/box7.jsx
@@ -0,0 +1,15 @@
+
+import "../styles/box7.css"
+export default function Section7Box() {
+    return (
+        <div className="box7">
+            <div className="row">
+                <div col-12>
+                    <h2>You are always </h2>
+                    <h2>welcome here if:</h2>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- src/components/section7.jsx
+++ src/components/section7.jsx
@@ -1,12 +1,13 @@
 
 import "../styles/section7.css"
+
 export default function SectionBox7() {
     return (
         <div className="section-box7">
             <div className="row">
 
                 <div className="col-lg-6 col-sm-12">
-
+                <SectionBox7/>``
                 </div>
                 <div id="img-box" className="col-lg-6 col-sm-12">
 
