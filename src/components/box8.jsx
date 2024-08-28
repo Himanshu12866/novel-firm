68192ee18cfce239c4394afbeea9353ad71d4202
Himanshu12866 <manshu010m@gmail.com>
2024-08-28 17:17:28 +0530

    "Updated Box8 component and styles, including HTML structure and CSS properties; also modified footer styles to change row width."

--- src/components/box8.jsx
+++ src/components/box8.jsx
@@ -10,16 +10,16 @@ export default function Box8() {
                 <p>Dubai Emirates</p>
                 <p>Phone Number : 789654123</p>
                 <p>Zip Code: 00000</p>
-                <p>Email : <i>Manshu010m@gmail.com</i></p>
+                <p style={{color:"white"}}>Email : <i>Manshu010m@gmail.com</i></p>
             </div>
             <div className="col-lg-6 col-sm-12">
                 <img src="../images/pic15.png" alt="dubai"></img>
                 <h3>In London</h3>
-                <p>Box No: 8</p>
-                <p>Dubai Emirates</p>
+                <p>35 Wood Line</p>
+                <p>Nottimgham</p>
                 <p>Phone Number : 789654123</p>
-                <p>Zip Code: 00000</p>
-                <p>Email : <i>Manshu010m@gmail.com</i></p>
+                <p>ZNP2 CPP4</p>
+                <p style={{color:"white"}}>Email : <i>Manshu010m@gmail.com</i></p>
             </div>
         </div>
     )
