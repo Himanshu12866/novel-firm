ec93b86ec3ce10f765f6dbae9993c7e3706eac4e
Himanshu12866 <manshu010m@gmail.com>
2024-08-28 17:39:35 +0530

    "Added Box9 component to Footer, updated footer.css to include padding"

---
+++ src/components/box9.jsx
@@ -0,0 +1,26 @@
+
+import "../styles/box9.css"
+export default function Box9() {
+    return (
+        <div className="row" id="Box-1d">
+            <div className="col-12">
+                <h2>Feel Free To Contact us</h2>
+                <h2>Anytime Anywhere</h2>
+                <p>No more fumbling to unlock your phone and find your
+                    notes app; Cheatsheet is as easy as pulling down
+                    center, pulling up the keyboard, or glancing at your
+                    Apple Watch.
+                </p>
+                <div className="row">
+                    <div className="col-9">
+                        <input type="text"></input>
+                    </div>
+                    <div className="col-3">
+                        <button>Subscribe</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,18 @@
 
 import "../styles/footer.css"
 import Box8 from "./box8"
-export default function Footer(){
+import Box9 from "./box9"
+export default function Footer() {
     return (
         <div className="footerBox">
             <div className="row">
                 <div className="col-lg-6 col-sm-12">
-                <Box8/>
+                    <Box8 />
 
                 </div>
-                <div className="col-lg-6 col-sm-12"></div>
+                <div className="col-lg-6 col-sm-12">
+                    <Box9 />
+                </div>
             </div>
         </div>
     )
