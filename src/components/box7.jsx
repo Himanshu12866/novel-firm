ccb8288c09d7ac9db100b83161174a70f93e1574
Himanshu12866 <manshu010m@gmail.com>
2024-08-28 15:37:28 +0530

    "Updated box7 component and styles: added new HTML structure, images, and CSS rules for layout, typography, and spacing."

--- src/components/box7.jsx
+++ src/components/box7.jsx
@@ -3,10 +3,42 @@ import "../styles/box7.css"
 export default function Section7Box() {
     return (
         <div className="box7">
-            <div className="row">
-                <div col-12>
+            <div className="row row1">
+                <div className="col-12">
                     <h2>You are always </h2>
                     <h2>welcome here if:</h2>
+                    <div className="insider-box">
+                        <div className="row">
+                            <div className="col-lg-2 col-sm-6">
+                                <img src="../images/png1.svg" alt="png"></img>
+                            </div>
+                            <div className="col-10">
+                                <p>No more fumbling to unlock your</p>
+                                <span>phone and find your notes app; </span>
+                            </div>
+
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-2 col-sm-6">
+                                <img src="../images/png2.svg" alt="png"></img>
+                            </div>
+                            <div className="col-10">
+                                <p>No more fumbling to unlock your</p>
+                                <span>phone and find your notes app; </span>
+                            </div>
+
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-2 col-sm-6">
+                                <img src="../images/png3.svg" alt="png"></img>
+                            </div>
+                            <div className="col-10">
+                                <p>No more fumbling to unlock your</p>
+                                <span>phone and find your notes app; </span>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
 
