28016e9efb0e9de6e5967383dc1d6d9bcb04263d
Himanshu12866 <manshu010m@gmail.com>
2024-08-28 15:19:05 +0530

    "Updated Section7 component and styles: renamed SectionBox7 to Section7Box, added new CSS rules for .box7 and .section-box7 classes, and updated CSS maps."

--- src/components/section7.jsx
+++ src/components/section7.jsx
@@ -1,5 +1,6 @@
 
 import "../styles/section7.css"
+import Section7Box from "./box7"
 
 export default function SectionBox7() {
     return (
@@ -7,7 +8,7 @@ export default function SectionBox7() {
             <div className="row">
 
                 <div className="col-lg-6 col-sm-12">
-                <SectionBox7/>``
+                <Section7Box/>
                 </div>
                 <div id="img-box" className="col-lg-6 col-sm-12">
 
